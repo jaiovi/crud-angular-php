f2e896b90926c7a4cf0fc946dc1e6b9173ddca0a
Jesús Sebastián Jaime Oviedo <a01412442@tec.mx>
2023-05-18 10:20:02 -0600

    Conexion con MongoDB

--- src/app/service/crud.service.ts
+++ src/app/service/crud.service.ts
@@ -7,26 +7,30 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class CrudService {
-  //API:string="http://localhost/libros/";
-  API:string="http://angular-test.eastus.cloudapp.azure.com/libros/";
+  API:string="http://localhost:3000/libros/";
+  //API:string="http://angular-test.eastus.cloudapp.azure.com/libros/";
   //API: string="angular-test.eastus.cloudapp.azure.com/libros/";
   constructor(private clientehttp:HttpClient) { 
     
   }
   AgregarLibro(datoslibro:Libro):Observable<any>{
     //se inserta datos de libro
-    return this.clientehttp.post(this.API+"?insertar=1",datoslibro);
+    //return this.clientehttp.post(this.API+"?insertar=1",datoslibro);
+    return this.clientehttp.post(this.API,datoslibro);
   }
   ObtenerLibros(){
     return this.clientehttp.get(this.API); //porque ya te lo da esrializado
   }
   BorrarLibro(isbn:any){
-    return this.clientehttp.get(this.API+"?borrar="+isbn);
+    //return this.clientehttp.get(this.API+"?borrar="+isbn);
+    return this.clientehttp.delete(this.API+isbn);
   }
   ObtenerLibro(isbn:any):Observable<any>{
-    return this.clientehttp.get(this.API+"?consultar="+isbn);
+    //return this.clientehttp.get(this.API+isbn);
+    return this.clientehttp.get(this.API+isbn);
   }
   EditarLibro(isbn:any, datosLibro:Libro):Observable<any>{
-    return this.clientehttp.post(this.API+"?actualizar="+isbn, datosLibro);
+    //return this.clientehttp.post(this.API+isbn, datosLibro);
+    return this.clientehttp.post(this.API+isbn, datosLibro);
   }
 }
